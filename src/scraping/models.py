efca7150784b8a05a1b0850f2ca13921cddd7c63
rza375 <v.fitisov@gmail.com>
2020-09-12 17:19:42 +0300

    add class Language in scraping_app

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -2,5 +2,23 @@ from django.db import models
 
 
 class City(models.Model):
-    name = models.CharField(max_length=50)
-    slug = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, verbose_name='Место вакансии', unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+    
+    class Meta:
+        verbose_name='Место ваканси'
+        verbose_name_plural = 'Название населенных пунктов'
+    
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Язык программирования', unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирвания'
+
+    def __str__(self):
+        return self.name
