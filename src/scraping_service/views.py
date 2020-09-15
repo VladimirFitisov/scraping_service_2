d8abf8482fef8dcffc3d2dbf74bd38498cc19f1c
rza375 <v.fitisov@gmail.com>
2020-09-15 23:05:04 +0300

    add base.html

--- src/scraping_service/views.py
+++ src/scraping_service/views.py
@@ -5,4 +5,4 @@ def home(request):
     date = datetime.datetime.now().date()
     name = 'ЗЛАТА'
     _context = {'date': date,'name': name}
-    return render(request, 'home.html', _context)
+    return render(request, 'base.html', _context)
