284e9bb9d7842d1b6cd902c56a230bb2cde08d78
Vladimir Fitisov <v.fitisov@gmail.com>
2020-10-08 19:56:40 +0300

    add new accounts/urls.py and login.html and new function login/logout

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+""" Функция входа и выхода
+https://docs.djangoproject.com/en/3.1/topics/auth/default/
+"""
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
+from accounts.forms import UsersLoginForm
+
+#функция входа
+def login_view(request):
+    form = UsersLoginForm(request.POST or None)
+    if form.is_valid():
+        #получение данных из формы
+        data = form.cleaned_data
+        email = data.get('email')
+        password = data.get('password')
+        #аутентификация usera
+        user = authenticate(request, email=email, password=password)
+        login(request, user)
+        return redirect('home')#после успешного заполнения формы, перенаправляем на главную страницу
+    return render(request, 'accounts/login.html', {'form': form}) #если форма не заполнена мы должны вернуть рендер и загрузить страничку с регистрацией
+# + создать файл accounts/urls для подключения данных вюшек
+#функция выхода
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
--- src/scraping/admin.py
+++ src/scraping/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 
+# Регистрация моделей в админке
 admin.site.register(City)
 admin.site.register(Language)
 admin.site.register(Vacancy)
