67438fed8cbecb6d29b03bd271625cde6bb9c54e
Vladimir Fitisov <v.fitisov@gmail.com>
2020-10-10 21:14:54 +0300

    add new function for registration new user

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -6,9 +6,10 @@ from django import forms
 from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.hashers import check_password
 
-User = get_user_model()# получение usera из базы данных
+User = get_user_model()  # получение usera из базы данных
 
 
+# Форма для входа пользователя
 class UserLoginForm(forms.Form):
     email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
@@ -22,11 +23,27 @@ class UserLoginForm(forms.Form):
             qs = User.objects.filter(email=email)
             if not qs.exists():
                 raise forms.ValidationError('Такого пользователя нет!')
-            if not check_password(password, qs[0].password): #преобразовывает пароль из текста и проверяет с существующем в база данных
+            if not check_password(password, qs[
+                0].password):  # преобразовывает пароль из текста и проверяет с существующем в база данных
                 raise forms.ValidationError('Пароль не верный')
             user = authenticate(email=email, password=password)
             if not user:
                 raise forms.ValidationError('Данный аккаунт отключен')
-        return super(UserLoginForm, self).clean(*args, **kwargs) #переопределение стандартной функции
+        return super(UserLoginForm, self).clean(*args, **kwargs)  # переопределение стандартной функции
 
 
+# Форма для регистрации нового пользователя
+class UserRegistrationform(forms.ModelForm):
+    email = forms.CharField(label='Введите email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Введите пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    class Meta():
+        model = User  # создаем форму на основе модели User
+        fields = ('email',)
+
+        def clean_password2(self):
+            data = self.cleaned_data
+            if data['password'] != data['password2']:
+                raise forms.ValidationError('Пароли не совпадают')
+            return data['password2']
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -5,24 +5,38 @@ https://docs.djangoproject.com/en/3.1/topics/auth/default/
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 
-from accounts.forms import UserLoginForm
+from accounts.forms import UserLoginForm, UserRegistrationform
 
-#функция входа
+
+# функция входа
 def login_view(request):
     form = UserLoginForm(request.POST or None)
     if form.is_valid():
-        #получение данных из формы
+        # получение данных из формы
         data = form.cleaned_data
         email = data.get('email')
         password = data.get('password')
-        #аутентификация usera
+        # аутентификация usera
         user = authenticate(request, email=email, password=password)
         login(request, user)
-        return redirect('home')#после успешного заполнения формы, перенаправляем на главную страницу
-    return render(request, 'accounts/login.html', {'form': form}) #если форма не заполнена мы должны вернуть рендер и загрузить страничку с регистрацией
+        return redirect('home')  # после успешного заполнения формы, перенаправляем на главную страницу
+    return render(request, 'accounts/login.html', {
+        'form': form})  # если форма не заполнена мы должны вернуть рендер и загрузить страничку с регистрацией
+
+
 # + создать файл accounts/urls для подключения данных вюшек
-#функция выхода
+# функция выхода
 
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+
+def register_view(request):
+    form = UserRegistrationform(request.POST or None)
+    if form.is_valid():
+        new_user = form.save(commit=False)
+        new_user.set_password(form.cleaned_data['password'])
+        new_user.save()
+        return render(request, 'accounts/register_done.html', {'new_user': new_user})
+    return render(request, 'accounts/register.html', {'form': form})
