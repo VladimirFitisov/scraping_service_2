958a69d7ef9a498ccbb4366726e3d22cbd3d67f8
Vladimir Fitisov <v.fitisov@gmail.com>
2020-10-12 21:15:32 +0300

    add messages user

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -5,9 +5,12 @@ https://docs.djangoproject.com/en/3.1/topics/auth/default/
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout, get_user_model
 
+from django.contrib import messages  # для вывода сообщений https://docs.djangoproject.com/en/3.1/ref/contrib/messages/
+
 from accounts.forms import UserLoginForm, UserRegistrationform, UserUpdateForm
 
 User = get_user_model()
+
 
 # функция входа
 def login_view(request):
@@ -39,6 +42,7 @@ def register_view(request):
         new_user = form.save(commit=False)
         new_user.set_password(form.cleaned_data['password'])
         new_user.save()
+        messages.success(request, 'Пользователь добавлен в систему')
         return render(request, 'accounts/register_done.html', {'new_user': new_user})
     return render(request, 'accounts/register.html', {'form': form})
 
@@ -55,6 +59,7 @@ def update_view(request):
                 user.language = data['language']
                 user.send_email = data['send_email']
                 user.save()
+                messages.success(request, 'Данные сохранены')
                 return redirect('accounts:update')
 
         form = UserUpdateForm(
@@ -72,4 +77,5 @@ def delete_view(request):
         if request.method == 'POST':
             qs = User.objects.get(pk=user.pk)
             qs.delete()
+            messages.error(request, 'Пользователь удален :(')
     return redirect('home')
