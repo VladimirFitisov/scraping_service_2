1a8b03abde4c66582b65929846f319a7d03bab24
rza375 <v.fitisov@gmail.com>
2020-10-23 23:43:28 +0300

    аdd refactoring send_email

--- src/send_emails.py
+++ src/send_emails.py
@@ -6,20 +6,49 @@ import os, sys
 import django
 
 from django.contrib.auth import get_user_model
+from django.core.mail import EmailMultiAlternatives
 
+from scraping_service.settings import EMAIL_HOST_USER
 
 proj = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(proj)
 os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
 
-
 django.setup()
+from scraping.models import Vacancy
 
-from django.core.mail import EmailMultiAlternatives
+subject = "Рассылка вакансий"
+text_content = "Рассылка вакансий"
+from_email = EMAIL_HOST_USER
+empty = '<h2>К сожалению на сегодня по Вашим предпочтениям данных нет.</h2>'
 
-subject, from_email, to = 'hello', 'from@example.com', 'to@example.com'
-# text_content = 'This is an important message.'#текстовое письмо
-html_content = '<p>This is an <strong>important</strong> message.</p>'# html-письмо
-msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-msg.attach_alternative(html_content, "text/html")
-msg.send()
+User = get_user_model()
+qs = User.objects.filter(send_email=True).values('city', 'language', 'email')
+users_dict = {}  # набор пользователя
+
+for i in qs:
+    users_dict.setdefault((i['city'], i['language']), [])
+    users_dict[(i['city'], i['language'])].append(i['email'])
+if users_dict:
+    params = {'city_id__in': [], 'language_id__in': []}  # выбрать все значения  которые принадлежат этой паре
+    for pair in users_dict.keys():
+        params['city_id__in'].append(pair[0])
+        params['language_id__in'].append(pair[1])
+    qs = Vacancy.objects.filter(**params).values()[:10]
+    vacancies = {}
+    for i in qs:
+        vacancies.setdefault((i['city_id'], i['language_id']), [])
+        vacancies[(i['city_id'], i['language_id'])].append(i)
+    for keys, emails in users_dict.items():
+        rows = vacancies.get(keys, [])
+        html = ''
+        for row in rows:
+            html += f'<h5><a href="{row["url"]}">{row["title"]}</a></h5>'
+            html += f'<p>{row["description"]}</p>'
+            html += f'<p>{row["company"]}</p><br><hr>'
+        _html = html if html else empty
+        for email in emails:
+            to = email
+            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg.attach_alternative(_html, "text/html")
+            msg.send()
